bdd474e5d32f5f7d1f7342cc2965799bc9e107ba
roshanghogale <126495884+roshanghogale@users.noreply.github.com>
2024-01-25 07:44:10 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -54,4 +54,60 @@ const questions = [
     options: ["int geeks[20];", "int geeks;", "geeks{20};", "array geeks[20];"],
     correctAnswer: "int geeks[20];",
   },
+  {
+    question: "Special symbol permitted with in the identifier name.",
+    options: ["$", "@", "_", "."],
+    correctAnswer: "_",
+  },
+  {
+    question:
+      "Which of the following is the correct extension of the Python file?",
+    options: [".python", ".pl", ".py", ".p"],
+    correctAnswer: ".py",
+  },
+  {
+    question:
+      "What will be the value of the following Python expression?\n4 + 3 % 5",
+    options: ["7", "2", "4", "1"],
+    correctAnswer: "7",
+  },
+  {
+    question:
+      "Which of the following is used to define a block of code in Python language?",
+    options: ["indentation", "key", "Brackets", "All of the mentioned"],
+    correctAnswer: "indentation",
+  },
+  {
+    question: " Which keyword is used for function in Python language?",
+    options: ["Function", "def", "Fun", "Define"],
+    correctAnswer: "def",
+  },
+  {
+    question:
+      "Python supports the creation of anonymous functions at runtime, using a construct called __________",
+    options: ["pi", "anonymous", "lambda", "none of the mentioned"],
+    correctAnswer: "lambda",
+  },
+  {
+    question:
+      "Which of the following is the truncation division operator in Python?",
+    options: ["|", "//", "/", "%"],
+    correctAnswer: "//",
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    options: ["<javascript>", "<scripting>", "<js>", "<script>"],
+    correctAnswer: "<script>",
+  },
+  {
+    question:
+      "What is the correct syntax for referring to an external script called demo.js?",
+    options: [
+      "<script src='demo.js'>",
+      "<script name='demo.js'>",
+      "<script src='value.js'>",
+      "<script href='demo.js'>",
+    ],
+    correctAnswer: "<script src='demo.js'>",
+  },
 ];
